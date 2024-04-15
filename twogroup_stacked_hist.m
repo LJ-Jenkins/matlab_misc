01a6fc645b78857300bf140944555c04345d81a0
Luke Jenkins <L.Jenkins@soton.ac.uk>
2024-04-15 17:30:26 +0100

    Create twogroup_stacked_hist.m

---
+++ twogroup_stacked_hist.m
@@ -0,0 +1,79 @@
+function [varargout] = twogroup_stacked_hist(var,bins,groups,opts,lgnd)
+%
+% plt.twogroup_stacked_hist - plots a histogram of a binned variable that is also stacked by a different grouping
+%
+% Inputs:
+%   var - data for binning
+%   bins - the bin edges, where bins(1) is the left edge of the first bin, and bins(end) is the right edge of the last bin
+%   groups - the groups from which to do the stacking (usually 1,2,3, etc)
+% -name-value arguments-
+%   'colors' - (required) a nx3 double of RBG triplets, 1 triplet for each group
+%   'percentages' - 'y' to plot the histogram as percentages (aka:'Normalisation','probability * 100)
+%   'addtofig' - 'y' adds plot to an open figure as the next tile
+%   'xlabel' - xlabel string
+%   'lgtitle' - title for the legend
+%    - any public property name and value for the legend() base function ,e.g., 'String',{'Label1','Label2'}
+%
+% Outputs (in order):
+%   1st - cell array of bar handles (one for each bar/bin)
+
+% Luke Jenkins Feb 2023
+% L.Jenkins@soton.ac.uk
+
+arguments
+    var (1,:) double
+    bins (1,:) double
+    groups (1,:) double
+    opts.colors
+    opts.percentages {mustBeMember(opts.percentages,{'y','n'})} = 'n'
+    opts.addtofig {mustBeMember(opts.addtofig,{'y','n'})} = 'n'
+    opts.xlabel string
+    opts.lgtitle string
+    lgnd.?matlab.graphics.illustration.Legend
+end
+
+lgnd = namedargs2cell(lgnd);
+bins = [bins ; 1:width(bins)];
+varg = nan(1,width(var));
+for i = 1:width(bins)-1
+    varg(1,var >= bins(1,i) & var < bins(1,i+1)) = bins(2,i);
+end
+if opts.addtofig == 'n'
+    figure('units','normalized','outerposition',[.225 .2 .55 .70]);
+end
+nexttile
+b = cell(1,width(bins)-1);
+for i = 1:width(bins)-1
+    if opts.percentages == 'y'
+        b{i} = bar(i,histcounts(groups(varg == bins(2,i)))/length(var)*100,'stacked');
+    else
+        b{i} = bar(i,histcounts(groups(varg == bins(2,i))),'stacked');
+    end
+    hold on
+    for j = 1:length(b{i})
+        b{i}(j).FaceColor = opts.colors(j,:);
+    end
+end
+xlbs = sls.plt.binlabels(bins(1,:));
+xlim([.5 length(b)+.5])
+xtick(1:length(xlbs)-1)
+xticklabels(xlbs(1:end-1))
+if opts.percentages ~= 'y'
+    ylabel('Counts')
+else
+    ylabel('Percentage (%)')
+end
+if isfield(opts,'xlabel')
+    xlabel(opts.xlabel);
+end
+[~,mxi] = max(cellfun(@(x) length(x), b));
+lg = legend(b{mxi},lgnd{:});
+if isfield(opts,'lgtitle')
+    title(lg,opts.lgtitle)
+end
+
+if nargout == 1
+    varargout{1} = b;
+end
+
+% fini
